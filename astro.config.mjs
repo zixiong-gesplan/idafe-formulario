eb01532993def491afd650ca3b028f89bcf498ca
Zixiong Lin Yang <zlinyan@gesplan.es>
2025-05-29 11:19:52 +0100

    Restore README and remove yaml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 import db from '@astrojs/db';
-import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zixiong-gesplan.github.io',
   base: 'idafe-formulario',
   vite: {
-    plugins: [tailwindcss(),yaml()],
+    plugins: [tailwindcss()],
   },
 
   integrations: [react(), db()],
