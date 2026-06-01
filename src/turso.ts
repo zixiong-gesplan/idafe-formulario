a7b63556d00467291221f4096cee46bd99bfa780
Zixiong Lin Yang | Gesplan <zlinyan@gesplan.es>
2026-06-01 16:03:13 +0100

    Updated keys

--- src/turso.ts
+++ src/turso.ts
@@ -2,5 +2,5 @@ import { createClient } from "@libsql/client/web";
 
 export const turso = createClient({
   url: import.meta.env.TURSO_DATABASE_URL || "libsql://idafe-zixiong.aws-eu-west-1.turso.io",
-  authToken: import.meta.env.TURSO_AUTH_TOKEN || "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJnaWQiOiI1NTEyNDI2Mi03MGQ2LTRmZjUtYmEwNC03NGQ3MGMxYjQzN2EiLCJpYXQiOjE3NDg1MDQ1MzcsInJpZCI6IjM4YzU2NmIyLTg5YWMtNDZkOS05ODJkLTk0NmRkZThiZmY1ZCJ9.riXVKtk6CLzPUDMzA75QQsCV04-vhjz0sRmTbAXQ6Eaf4eXWLKTFPET00pp242quUeO7PHF_lUtIdNthDBsxAw",
+  authToken: import.meta.env.TURSO_AUTH_TOKEN || "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJleHAiOjE3ODI5MTgxNjgsImlhdCI6MTc4MDMyNjE2OCwiaWQiOiI4NjBkOTcyMS00ZTRkLTRhODQtYWZiOC0yMDYzMGRjMTJmNzYiLCJyaWQiOiI0NTYxYzZmZi1jZTY3LTQwZGYtYjM2MC03NDVjYjk2YmRiNzYifQ.3ix1wc3i4KgmA9XeaWEXZZLcRu_qN1HUQOJfMrNReKaoIK09D7fRfd9IuMePzd_Spbapwt1YvHLxozcEjgFNAg",
 });
